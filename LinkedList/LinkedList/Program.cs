4936437536048f2369d4b5f675b9d3ab7b127287
Ronmel <ronmelross@gmail.com>
2019-03-22 13:29:21 +0800

    002 - Binary Search Tree

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,21 +1,31 @@
 ﻿using System;
+
 class Node
 {
-    public int data;
     public Node next;
+    public int data;
     public Node(int d)
     {
         data = d;
         next = null;
     }
-
 }
+
+class NodeBST
+{
+    public NodeBST left, right;
+    public int data;
+    public NodeBST(int data)
+    {
+        this.data = data;
+        left = right = null;
+    }
+}
+
 class Solution
 {
-
     public static Node insert(Node head, int data)
     {
-        //Recursion
         if (head == null)
         {
             return new Node(data);
@@ -25,11 +35,11 @@ class Solution
             head.next = new Node(data);
         } else
         {
+            //recursion
             insert(head.next, data);
         }
         return head;
     }
-
     public static void display(Node head)
     {
         Node start = head;
@@ -39,17 +49,71 @@ class Solution
             start = start.next;
         }
     }
+
+    static NodeBST insert(NodeBST root, int data)
+    {
+        if (root == null)
+        {
+            return new NodeBST(data);
+        }
+        else
+        {
+            NodeBST cur;
+            if (data <= root.data)
+            {
+                cur = insert(root.left, data);
+                root.left = cur;
+            }
+            else
+            {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+    static int getHeight(NodeBST root)
+    {
+        //Write your code here
+        if (root==null)
+        {
+            return -1;
+        }
+        int leftDepth = getHeight(root.left);
+        int rightDepth = getHeight(root.right);
+
+        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+    }
+
     static void Main(String[] args)
     {
-
+        #region Day15: Linked List
+        Console.WriteLine("LINKED LIST");
         Node head = null;
+        Console.Write("No. of entries: ");
         int T = Int32.Parse(Console.ReadLine());
-        while (T-->0)
+        while (T-- > 0)
         {
             int data = Int32.Parse(Console.ReadLine());
             head = insert(head, data);
         }
         display(head);
+        #endregion
+
+        #region Day22: Binary Search Trees
+        Console.WriteLine("\n\nBINARY SEARCH TREES");
+        NodeBST root = null;
+        Console.Write("No. of entries: ");
+        int input = Int32.Parse(Console.ReadLine());
+        while (input -- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            root = insert(root, data);
+        }
+        int height = getHeight(root);
+        Console.WriteLine(height);
+        #endregion
+
         Console.ReadKey();
     }
 }
